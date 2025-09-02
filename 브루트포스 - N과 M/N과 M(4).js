660c4c2bb22085a7f5ba64d49c7b35a5a59ca628
김명원 <skqjahjakskxl@gmail.com>
2025-09-02 17:29:04 +0900

    브루트 포스 N과 M(5)

---
+++ 브루트포스 - N과 M/N과 M(4).js
@@ -0,0 +1,29 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+const N = input[0],
+  M = input[1];
+
+const arr = [];
+const result = [];
+
+function dfs(start, depth) {
+  if (depth === M) {
+    result.push(arr.join(" "));
+    return;
+  }
+
+  for (let i = start; i <= N; i++) {
+    arr.push(i);
+    dfs(i, depth + 1);
+    arr.pop();
+  }
+}
+
+dfs(1, 0);
+
+console.log(result.join("\n"));
